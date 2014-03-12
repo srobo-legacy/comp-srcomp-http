3bfa6f981d9e1badbf20c864eca6700b091f1337
Rob Spanton <rspanton@zepler.net>
2014-03-12 02:25:46 +0000

    Add app.py from which-match-is-it
    With a modification to permit pointing it at a separate compstate
    repository.

---
+++ app.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+import os
+from flask import g, Flask, jsonify, json
+from srcomp import SRComp
+import time
+
+ROOTDIR = os.path.dirname(__file__)
+
+app = Flask(__name__)
+
+class SRCompManager(object):
+    def __init__(self):
+        self.root_dir = "./"
+        self.update_time = None
+
+    def _load(self):
+        self.comp = SRComp(self.root_dir)
+        self.update_time = time.time()
+
+    def get_comp(self):
+        if self.update_time is None:
+            self._load()
+
+        elif time.time() - self.update_time > 5:
+            "Data is more than 5 seconds old -- reload"
+            self._load()
+
+        return self.comp
+
+comp_man = SRCompManager()
+
+@app.before_request
+def before_request():
+    if "COMPSTATE" in app.config:
+        comp_man.root_dir = app.config["COMPSTATE"]
+    g.comp_man = comp_man
+
+def match_json_info(match):
+    return {
+        "number": match.num,
+        "arena": match.arena,
+        "teams": match.teams,
+        "start_time": match.start_time.isoformat(),
+        "end_time": match.end_time.isoformat(),
+    }
+
+@app.route("/matches/<arena>/<int:match_number>")
+def match_info(arena, match_number):
+    "Get information about the given match number"
+    comp = g.comp_man.get_comp()
+
+    if match_number not in comp.schedule.matches:
+        return jsonify(error=True,msg="Invalid match number"), 404
+    match = comp.schedule.matches[match_number]
+
+    if arena not in match:
+        return jsonify(error=True,msg="Invalid arena"), 404
+    match = match[arena]
+
+    return jsonify(**match_json_info(match))
+
+@app.route("/matches/<arena>/<int:match_number_min>-<int:match_number_max>")
+def match_info_range(arena, match_number_min, match_number_max):
+    "Get information about the given range of matches"
+    comp = g.comp_man.get_comp()
+    resp = {}
+
+    for match_n in range(match_number_min, match_number_max+1):
+        if match_n not in comp.schedule.matches:
+            "Skip matches that don't exist"
+            continue
+
+        resp[match_n] = match_json_info(comp.schedule.matches[match_n][arena])
+
+    return json.dumps(resp)
+
+@app.route("/matches/<arena>/current")
+def current_match_info(arena):
+    comp = g.comp_man.get_comp()
+    current = comp.schedule.current_match()
+
+    if current is None:
+        return jsonify(number=None, msg="No current match")
+
+    if arena not in current:
+        return jsonify(error=True,msg="Invalid arena"), 404
+    match = current[arena]
+
+    return jsonify(**match_json_info(match))
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser( description = "SR Competition info API HTTP server" )
+    parser.add_argument("compstate", help = "Competition state git repository path")
+    parser.add_argument("-p", "--port", type=int, default=5112,
+                        help = "Port to listen on")
+    args = parser.parse_args()
+
+    app.config["COMPSTATE"] = args.compstate
+    app.run(host='0.0.0.0', port=args.port)
