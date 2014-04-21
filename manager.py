0ccc9e93ad6e9bf7c47b1b7f62e29d1ef0f5f3cd
Peter Law <PeterJCLaw@gmail.com>
2014-04-21 16:18:07 +0100

    Add a compstate update mechanism.
    During normal operation, the http backend will (at most once each
    5 seconds) look for a specific file in the compstate repo. If this
    file is present, nothing happens (ie it uses the data it already has).
    If the file is missing, then it will create and lock the file and
    then re-load the SRComp instance.
    
    Updates to the compstate should be done using the provided 'update'
    script. This will fetch the latest revisions from the 'origin' remote,
    acquire a lock on the same file and then checkout origin/master.
    Once complete, the script deletes the file.
    
    As the http process will only re-load the compstate after the file
    has been removed, and the file only gets removed when the update is
    complete, it may appear that the locks aren't needed. However, there
    are two cases where the file is not present:
      * initial startup
      * after an update
    During these times, there are potential race conditions between the
    data being loaded and it being updated. The locks ensure that even
    here, access to the state is restricted and suitably ordered.
    
    This is related to #2363 (Build updating of competition state), but
    doesn't resolve it.
    
    Change-Id: Ibee155a571b6f62d5470bffade3b0c4df95a0f15

--- manager.py
+++ manager.py
@@ -1,7 +1,37 @@
 
+import fcntl
+import os
 import time
 
 from srcomp import SRComp
+
+LOCK_FILE = ".update-lock"
+
+def acquire_lock(lock_path):
+    fd = open(lock_path, "w")
+    fcntl.flock(fd, fcntl.LOCK_EX)
+    return fd
+
+class SelfDeletingLockableFile(object):
+    def __init__(self, path):
+        self._path = path
+
+    def __enter__(self):
+        self._fd = acquire_lock(self._path)
+        return self._fd
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._fd.close()
+        os.remove(self._path)
+
+def update_lock(compstate_path):
+    """ Acquire a lock on the given compstate for the purposes of
+        updating it. Returns a context manager object which will remove
+        the lock and file when __exit__ed. In turn that triggers the
+        manager to re load the information it has.
+    """
+    lock_path = os.path.join(compstate_path, LOCK_FILE)
+    return SelfDeletingLockableFile(lock_path)
 
 class SRCompManager(object):
     def __init__(self):
@@ -9,16 +39,25 @@ class SRCompManager(object):
         self.update_time = None
 
     def _load(self):
-        self.comp = SRComp(self.root_dir)
-        self.update_time = time.time()
+        with acquire_lock(self.lock_path):
+            "grab a lock & reload"
+            #print "Loading compstate from {0}".format(self.root_dir)
+            self.comp = SRComp(self.root_dir)
+            self.update_time = time.time()
+
+    def _state_changed(self):
+        return not os.path.exists(self.lock_path)
+
+    @property
+    def lock_path(self):
+        return os.path.join(self.root_dir, LOCK_FILE)
 
     def get_comp(self):
         if self.update_time is None:
             self._load()
 
-        elif time.time() - self.update_time > 5:
-            "Data is more than 5 seconds old -- reload"
+        elif time.time() - self.update_time > 5 and self._state_changed():
+            "Data is more than 5 seconds old and the state has changed -- reload"
             self._load()
 
         return self.comp
-
