b61b24461ba69af8cbb260da4e7dca5e2b03c99d
Peter Law <PeterJCLaw@gmail.com>
2014-04-21 16:18:07 +0100

    Add a compstate update mechanism.
    During normal operation, the http backend will (at most once each
    5 seconds) look for a specific file in the compstate repo. If this
    file has not been touched since the last time it checked, nothing
    happens (ie it uses the data it already has). If the file has a
    modification time that is different, then it will get a lock on the
    competition state repo (using a different file) and then re-load the
    SRComp instance.
    
    Updates to the compstate should be done using the provided 'update'
    script. This will fetch the latest revisions from the 'origin' remote,
    acquire the same lock on the competition state repo and then checkout
    origin/master. The script then touches the update file and releases
    the lock.
    
    By keeping the signalling separate from the locking we avoid race
    conditions in accessing the lock file, while also providing a simple
    way to detect that updates have occcured.
    
    This is related to #2363 (Build updating of competition state), but
    doesn't resolve it.
    
    Change-Id: Ibee155a571b6f62d5470bffade3b0c4df95a0f15

--- manager.py
+++ manager.py
@@ -1,24 +1,80 @@
 
+import contextlib
+import fcntl
+import os
 import time
 
 from srcomp import SRComp
 
+LOCK_FILE = ".update-lock"
+UPDATE_FILE = ".update-pls"
+
+def update_lock_path(compstate_path):
+    return os.path.join(compstate_path, LOCK_FILE)
+
+def update_pls_path(compstate_path):
+    return os.path.join(compstate_path, UPDATE_FILE)
+
+def acquire_lock(lock_path):
+    fd = open(lock_path, "w")
+    fcntl.lockf(fd, fcntl.LOCK_EX)
+    return fd
+
+def touch_update_file(compstate_path):
+    file_path = update_pls_path(compstate_path)
+    open(file_path, 'w').close()
+
+@contextlib.contextmanager
+def update_lock(compstate_path):
+    """ Acquire a lock on the given compstate for the purposes of
+        updating it. Returns a context manager object which will remove
+        the lock and file when __exit__ed. In turn that triggers the
+        manager to re load the information it has.
+    """
+    lock_path = update_lock_path(compstate_path)
+    with acquire_lock(lock_path):
+        yield
+        touch_update_file(compstate_path)
+
 class SRCompManager(object):
     def __init__(self):
         self.root_dir = "./"
+
+        # The last time we updated our information
         self.update_time = None
 
+        # The time the update pls file was last modified
+        self._update_pls_time = None
+
     def _load(self):
-        self.comp = SRComp(self.root_dir)
-        self.update_time = time.time()
+        lock_path = update_lock_path(self.root_dir)
+        with acquire_lock(lock_path):
+            "grab a lock & reload"
+            #print "Loading compstate from {0}".format(self.root_dir)
+            self.comp = SRComp(self.root_dir)
+            self.update_time = time.time()
+
+    def _state_changed(self):
+        update_path = update_pls_path(self.root_dir)
+        try:
+            new_time = os.path.getmtime(update_path)
+        except OSError:
+            # It doesn't exist. That's fine -- use a value which won't ever
+            # happen so that we load at least the first time through
+            new_time = -1
+
+        if new_time != self._update_pls_time:
+            self._update_pls_time = new_time
+            return True
+
+        return False
 
     def get_comp(self):
         if self.update_time is None:
             self._load()
 
-        elif time.time() - self.update_time > 5:
-            "Data is more than 5 seconds old -- reload"
+        elif time.time() - self.update_time > 5 and self._state_changed():
+            "Data is more than 5 seconds old and the state has changed -- reload"
             self._load()
 
         return self.comp
-
