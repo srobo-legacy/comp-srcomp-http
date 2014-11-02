7172c9e7ad184b5b89cbf465d6f260d0ab15b888
Alistair Lynn <arplynn@gmail.com>
2014-11-02 12:32:22 +0000

    Switch to a more consistent REST API
    Featuring some actual REST principles. Major points of note:
    
    * Scoring is now just part of the team information,
    * Matches come through one endpoint which can be filtered,
    * Arenas, teams and corners have an associated URI,
    * The API is (mostly) discoverable.
    
    Patch Set #3: Refactor filter; add number and time filtering
    
    Change-Id: I9e7ff3f5a251cb8ca48383c9c8feedc429f3de4e

--- sr/comp/http/server.py
+++ sr/comp/http/server.py
@@ -1,5 +1,5 @@
-
-from flask import g, Flask, jsonify, request
+import dateutil.parser
+from flask import g, Flask, jsonify, request, url_for
 
 from sr.comp.http.manager import SRCompManager
 from sr.comp.http.query_utils import match_json_info, match_parse_name
@@ -20,124 +20,60 @@ def after_request(resp):
         resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-@app.route("/matches/<arena>", methods=["GET"])
-def match_query(arena):
+@app.route('/')
+def root():
+    return jsonify(arenas=url_for('arenas'),
+                   teams=url_for('teams'),
+                   corners=url_for('corners'),
+                   state=url_for('state'),
+                   matches=url_for('matches'))
+
+@app.route('/arenas')
+def arenas():
     comp = g.comp_man.get_comp()
 
-    if "numbers" in request.args:
-        result = []
+    return jsonify(arenas={name: url_for('get_arena', arena=name)
+                            for name in comp.arenas})
 
-        for desc in request.args["numbers"].split(","):
-            try:
-                m = match_parse_name(comp.schedule, arena, desc)
-            except (IndexError, KeyError, ValueError):
-                return jsonify(error=True,
-                               msg="Unknown match '{0}'".format(desc)), 400
-            info = match_json_info(comp, m)
-            info["query"] = desc
-            result.append(info)
+@app.route('/arenas/<arena>')
+def get_arena(arena):
+    return jsonify(name=arena)
 
-        return jsonify(matches=result)
-    return jsonify(error=True), 400
+def team_info(comp, team):
+    scores = comp.scores.league.teams[team.tla]
+    return {'name': team.name,
+            'get': url_for('get_team', tla=team.tla),
+            'tla': team.tla,
+            'scores': {'league': scores.league_points,
+                       'game': scores.game_points}}
 
-@app.route("/matches/<arena>/<int:match_number>")
-def match_info(arena, match_number):
-    "Get information about the given match number"
-    comp = g.comp_man.get_comp()
 
-    if match_number < 0 or match_number > len(comp.schedule.matches):
-        return jsonify(error=True,msg="Invalid match number"), 404
-    match = comp.schedule.matches[match_number]
-
-    if arena not in match:
-        return jsonify(error=True,msg="Invalid arena"), 404
-    match = match[arena]
-
-    return jsonify(**match_json_info(comp, match))
-
-@app.route("/matches/<arena>/<int:match_number_min>-<int:match_number_max>")
-def match_info_range(arena, match_number_min, match_number_max):
-    "Get information about the given range of matches"
-    comp = g.comp_man.get_comp()
-    resp = {}
-
-    match_number_min = max(match_number_min, 0)
-    match_number_max = min(match_number_max, len(comp.schedule.matches)-1)
-
-    for match_n in range(match_number_min, match_number_max+1):
-        resp[match_n] = match_json_info(comp, comp.schedule.matches[match_n][arena])
-
-    return jsonify(matches = resp)
-
-@app.route("/matches/all")
-def match_info_all():
-    "Get information about all the scheduled matches"
-    comp = g.comp_man.get_comp()
-
-    return jsonify(matches = comp.schedule.matches)
-
-@app.route("/matches/periods")
-def match_info_periods():
-    "Get information about all the scheduled matches, organised by period"
-    comp = g.comp_man.get_comp()
-
-    return jsonify(periods = comp.schedule.match_periods)
-
-@app.route("/matches/knockouts")
-def knockout_matches():
-    "Get information about all the knockout matches"
-    comp = g.comp_man.get_comp()
-
-    return jsonify(rounds = comp.schedule.knockout_rounds)
-
-@app.route("/matches/<arena>/current")
-def current_match_info(arena):
-    comp = g.comp_man.get_comp()
-    current = comp.schedule.current_match(arena)
-
-    if current is None:
-        return jsonify(number=None, msg="No current match")
-
-    return jsonify(**match_json_info(comp, current))
-
-@app.route("/teams")
+@app.route('/teams')
 def teams():
     comp = g.comp_man.get_comp()
 
     resp = {}
     for team in comp.teams.values():
-        resp[team.tla] = team.name
+        resp[team.tla] = team_info(comp, team)
 
-    return jsonify(teams = resp)
+    return jsonify(teams=resp)
 
-@app.route("/scores/league")
-def scores():
+@app.route('/teams/<tla>')
+def get_team(tla):
     comp = g.comp_man.get_comp()
 
-    league = {}
-    game = {}
-    for tla, pts in comp.scores.league.teams.items():
-        league[tla] = pts.league_points
-        game[tla] = pts.game_points
+    team = comp.teams[tla]
 
-    return jsonify(league_points=league, game_points=game, \
-                    last_scored=comp.scores.league.last_scored_match)
-
-@app.route("/arenas")
-def arenas():
-    comp = g.comp_man.get_comp()
-
-    return jsonify(arenas = comp.arenas)
-
+    return jsonify(team_info(comp, team))
 
 @app.route("/corners")
 def corners():
     comp = g.comp_man.get_comp()
-    return jsonify(corners=comp.corners)
+    return jsonify(corners={number: url_for('get_corner', number=number)
+                              for number in comp.corners})
 
-
-@app.route("/corner/<int:number>")
-def corner(number):
+@app.route("/corners/<int:number>")
+def get_corner(number):
     comp = g.comp_man.get_comp()
 
     if number not in comp.corners:
@@ -146,7 +82,65 @@ def corner(number):
     return jsonify(**comp.corners[number].__dict__)  # **Corner doens't work
 
 @app.route("/state")
-def state_label():
+def state():
     comp = g.comp_man.get_comp()
     return jsonify(state = comp.state)
 
+
+def parse_difference_string(string, type_converter=int):
+    """
+    Parse a difference string (x..x, ..x, x.., x) and return a function that
+    accepts a single argument and returns ``True`` if it is in the difference.
+    """
+    separator = '..'
+    tokens = string.split(separator)
+
+    if len(tokens) > 2:
+        raise ValueError('Argument is not a different string.')
+    elif len(tokens) == 1:
+        converted_token = type_converter(tokens[0])
+        return lambda x: x == converted_token
+    elif len(tokens) == 2:
+        if not tokens[1]:
+            lower_bound = type_converter(tokens[0])
+            return lambda x: x >= lower_bound
+        elif not tokens[0]:
+            upper_bound = type_converter(tokens[1])
+            return lambda x: x <= upper_bound
+        else:
+            lhs = type_converter(tokens[0])
+            rhs = type_converter(tokens[1])
+            if lhs > rhs:
+                raise ValueError('Bounds are the wrong way around.')
+            return lambda x: lhs <= x <= rhs
+    else:
+        raise AssertionError('Argument contains unknown input.')
+
+
+@app.route("/matches")
+def matches():
+    comp = g.comp_man.get_comp()
+    matches = []
+    for slots in comp.schedule.matches:
+        matches.extend(match_json_info(comp, match) for match in slots.values())
+
+    def parse_date(string):
+        if ' ' in string:
+            raise ValueError('Date string should not contain spaces. '
+                             "Did you pass in a '+'?")
+        return dateutil.parser.parse(string)
+
+    filters = [
+        ('type', str),
+        ('arena', str),
+        ('num', int),
+        ('start_time', parse_date),
+        ('end_time', parse_date)
+    ]
+
+    for filter_key, filter_type in filters:
+        if filter_key in request.args:
+            predicate = parse_difference_string(request.args[filter_key], filter_type)
+            matches = [match for match in matches if predicate(filter_type(match[filter_key]))]
+
+    return jsonify(matches=matches)
