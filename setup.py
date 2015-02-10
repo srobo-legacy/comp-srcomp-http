733fbd359cf6d7b84290faa71425172d9798df81
Tom Leese <inbox@tomleese.me.uk>
2015-02-10 18:40:46 +0000

    Add Sphinx as a setup.py dependency

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open('README.rst') as f:
     long_description = f.read()
 
 
-setup(name="sr.comp.http",
-      version="1.0.0",
+setup(name='sr.comp.http',
+      version='1.0.0',
       packages=find_packages(),
       namespace_packages=['sr', 'sr.comp'],
       package_data={'sr.comp.http': ['logging-*.ini']},
@@ -20,5 +20,6 @@ setup(name="sr.comp.http",
                         'Flask >=0.10, <0.11',
                         'simplejson >=3.6, <4',
                         'python-dateutil >=2.2, <3'],
-      setup_requires=['nose >=1.3, <2'],
+      setup_requires=['nose >=1.3, <2',
+                      'Sphinx >=1.2, <2'],
       test_suite='nose.collector')
