510f926df93ebc4fce7d1a18ded18c12a77cab31
Tom Leese <inbox@tomleese.me.uk>
2015-04-18 07:56:17 +0100

    Fix tests for new knockout ranking
    This was accidentally missed when the patch went through.

--- tests/test_query_utils.py
+++ tests/test_query_utils.py
@@ -3,9 +3,11 @@ import mock
 from sr.comp.http.query_utils import get_scores
 from sr.comp.match_period import Match, MatchType
 
+
 GAME_POINTS_DUMMY = 'test game_points for: '
 POSITIONS_DUMMY = 'test positions for: '
 RANKED_DUMMY = 'test ranked for: '
+
 
 def make_session_scores(arena, num):
     key = (arena, num)
@@ -13,7 +15,9 @@ def make_session_scores(arena, num):
     scores.game_points = {key: GAME_POINTS_DUMMY + str(key)}
     scores.game_positions = {key: {num: [POSITIONS_DUMMY + repr(arena)]}}
     scores.ranked_points = {key: RANKED_DUMMY + str(key)}
+    scores.resolved_positions = {key: {POSITIONS_DUMMY + repr(arena): num}}
     return scores
+
 
 def build_scores():
     scores = mock.Mock()
@@ -22,14 +26,17 @@ def build_scores():
     scores.tiebreaker = make_session_scores('A', 2)
     return scores
 
-def build_match(num, arena, teams = None, start_time = None, \
-                end_time = None, type_ = None):
-    return Match(num, 'Match {n}'.format(n=num), arena, teams,
-                 start_time, end_time, type_)
+
+def build_match(num, arena, teams=None, start_time=None, end_time=None,
+                type_=None):
+    return Match(num, 'Match {n}'.format(n=num), arena, teams, start_time,
+                 end_time, type_)
+
 
 def test_league_match():
     scores = build_scores()
-    info = get_scores(scores, build_match(num=0, arena='A', type_=MatchType.league))
+    info = get_scores(scores, build_match(num=0, arena='A',
+                                          type_=MatchType.league))
     expected = {
         "game": GAME_POINTS_DUMMY + "('A', 0)",
         "league": RANKED_DUMMY + "('A', 0)",
@@ -37,9 +44,11 @@ def test_league_match():
     }
     assert expected == info
 
+
 def test_knockout_match():
     scores = build_scores()
-    info = get_scores(scores, build_match(num=1, arena='A', type_=MatchType.knockout))
+    info = get_scores(scores, build_match(num=1, arena='A',
+                                          type_=MatchType.knockout))
     expected = {
         "game": GAME_POINTS_DUMMY + "('A', 1)",
         "normalised": RANKED_DUMMY + "('A', 1)",
@@ -47,15 +56,18 @@ def test_knockout_match():
     }
     assert expected == info
 
+
 def test_tiebreaker_match():
     scores = build_scores()
-    info = get_scores(scores, build_match(num=2, arena='A', type_=MatchType.tiebreaker))
+    info = get_scores(scores, build_match(num=2, arena='A',
+                                          type_=MatchType.tiebreaker))
     expected = {
         "game": GAME_POINTS_DUMMY + "('A', 2)",
         "normalised": RANKED_DUMMY + "('A', 2)",
         "ranking": { POSITIONS_DUMMY + "'A'": 2 },
     }
     assert expected == info
+
 
 def test_no_match():
     scores = build_scores()
