dde74b2d5b48357cace4f96d592433f47054e599
Peter Law <PeterJCLaw@gmail.com>
2016-11-13 20:02:37 +0000

    Add missing call to super __init__
    Change-Id: I22c03b3949b9811b325de906a48e00cbbbacc71d

--- sr/comp/http/errors.py
+++ sr/comp/http/errors.py
@@ -6,4 +6,5 @@ class UnknownMatchFilter(BadRequest):
     description = 'Unknown match filter.'
 
     def __init__(self, name):
+        super(UnknownMatchFilter, self).__init__()
         self.details = {'name': name}
